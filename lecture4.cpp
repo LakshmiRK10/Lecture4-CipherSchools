574e83fa90b49cd77e40d37e40631dc1bb82b55a
LakshmiRK10 <121750915+LakshmiRK10@users.noreply.github.com>
2024-06-29 21:07:18 +0530

    Add files via upload

---
+++ lecture4.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    //Declare 2 variables and then assign the values of 3,5 and then add them
+    int a;
+    int b;
+
+   // a=3;
+   // b=5;
+
+    int c;
+    c=a+b;
+
+    cout<<c;
+
+}
---
+++ lecture42.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    //Declare 2 variables and then assign the values of 3,5 and then add them
+    int a=3;
+    int b=5;
+
+   
+ cout<<a+b<<endl<<"a+b";
+
+}
